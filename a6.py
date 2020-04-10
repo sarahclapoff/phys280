7369f489177944880dba1a249757ad8d4d64fd4b
sarahclapoff <63381231+sarahclapoff@users.noreply.github.com>
2020-04-10 01:18:36 -0700

    q1 attempt1

--- a6.py
+++ a6.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Qubit chaining for 3-SAT
+# #  Physics 280: A6
+
+# # 1. Qubit chaining for 3-SAT
 
 # In class we described a minor-embedding procedure to
 # implement 3-qubit 3-SAT (logical qubits a, b, c) with 4 physical qubits (q0, q1, q4, q5).
@@ -16,8 +18,38 @@
 # Hint: Use the sampler ExactSolver from dimod to generate a list of all possibilities.
 # 
 
+# In[2]:
+
+
+#import dwave stuff
+import dimod
+
+
+# In[6]:
+
+
+#like normal implementation except using cpu (exactsolver) instead of qpu (the other sampler)
+#use chaining to lock q0,q5 and set biases and strengths
+
+#look at it classically, map to chimera using 4 qubits (lecture notes 'Minor Embedding')
+#since b=q0=q5 do i need all 4 or just three? I think all 4 bc still doing it through leap?
+
+#(format it nice from notes) bias of a,c = -1/3; bias of b = 1/3
+biases = {(0,0): 1/3, (1,1): -1/3, (4,4): -1/3, (5,5): 1/3}
+#(5,0) should have a high strength (so really low number) bc they're bonded together
+strengths = {(0,5): -1, (0,4): 2/3, (1,4): 2/3, (1,5): 2/3}
+
+#dictionary of qubit biases and strengths
+Q = dict(biases)
+Q.update(strengths)
+
+#implement on "classical" exact solver
+classical_result = dimod.ExactSolver().sampleset(Q)
+print(classical_result)
+
+
 # In[ ]:
 
 
-print("test")
+
 
